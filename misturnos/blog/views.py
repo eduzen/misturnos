70f5c8036fdb07fcf71b2b5524f0e0cc75718e12
mOtz <killboymota@gmail.com>
2015-12-18 12:01:07 -0300

    working copy

--- misturnos/blog/views.py
+++ misturnos/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +13,8 @@ def post_list(request):
 def post_detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
