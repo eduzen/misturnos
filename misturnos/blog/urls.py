b9e375636e043e69dc2149902538bceb9e79cf75
eduzen <eduardo.a.enriquez@gmail.com>
2015-12-20 20:04:19 -0300

    Added several things

--- misturnos/blog/urls.py
+++ misturnos/blog/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^index$', views.index),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
--- misturnos/blog/views.py
+++ misturnos/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
 
 
-# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
 
 
 def post_list(request):
