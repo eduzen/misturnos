3dec48bf88017b294a464006ea823b4fdfb1bb9d
mOtz <killboymota@gmail.com>
2015-12-21 18:02:02 -0300

    added test template

--- misturnos/blog/views.py
+++ misturnos/blog/views.py
@@ -45,3 +45,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def test(request):
+    return render(request, 'blog/test.html')
