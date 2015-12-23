bfc70851762a885bfbdf130bc9ffa62d085906ac
eduzen <eduardo.a.enriquez@gmail.com>
2015-12-23 16:54:23 -0300

    added address model

--- misturnos/blog/models.py
+++ misturnos/blog/models.py
@@ -28,34 +28,6 @@ class Post(models.Model):
         return self.title
 
 
-class register(View):
-    def post(self, request, *args, **kwargs):
-        print "\n edu"
-        print request.POST['username']
-        print request.POST['password']
-
-        user = auth.authenticate(
-            username=request.POST['username'],
-            password=request.POST['password']
-        )
-        if user is not None:
-            if user.is_active:
-                auth.login(request, user)
-            else:
-                messages.error(request, 'Account not available.')
-        else:
-            messages.error(
-                request,
-                'Password incorrect or account not available.'
-            )
-        return HttpResponseRedirect('/misturnos')
-
-    def get(request, *args, **kwargs):
-        print "\ get de login"
-        # we should never get to this codepath
-        return render(request, 'taskmanager/login.html')
-
-
 class Profile(models.Model):
     # Relations
     user = models.OneToOneField(
@@ -64,10 +36,7 @@ class Profile(models.Model):
         verbose_name=_("user"),
         on_delete=models.CASCADE
         )
-    # Attributes - Mandatory
-
-    # Attributes - Optional
-    # Object Manager
+    # Attributes
 
     # Custom Properties
     @property
@@ -99,18 +68,8 @@ class Project(models.Model):
         verbose_name=_("name"),
         help_text=_("Ingrese el nombre del proyecto")
         )
-    color = models.CharField(
-        max_length=7,
-        default="#fff",
-        validators=[RegexValidator(
-            "(^#[0-9a-fA-F]{3}$)|(^#[0-9a-fA-F]{6}$)")],
-        verbose_name=_("color"),
-        help_text=_("Enter the hex color code, like #ccc or #cccccc")
-        )
     # Attributes - Optional
-    # Object Manager
-    objects = managers.ProjectManager()
-    # Custom Properties
+
     # Methods
 
     # Meta and String
@@ -124,7 +83,7 @@ class Project(models.Model):
         return "%s - %s" % (self.user, self.name)
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def create_profile_for_new_user(sender, created, instance, **kwargs):
     if created:
         profile = Profile(user=instance)
